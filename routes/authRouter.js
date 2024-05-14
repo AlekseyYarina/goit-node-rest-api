1ac90645b2bff7ed30004492eff0ce62b0ce940f
Yarina Oleksii <aleksey.yarina@gmail.com>
2024-05-14 17:37:24 +0300

    add: register, login Joi

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,14 +1,35 @@
 import express from "express";
 import AuthController from "../controllers/authControllers.js";
-
+import validateBody from "../helpers/validateBody.js";
 import authMiddleware from "../middleware/auth.js";
+
+import {
+  registerSchema,
+  loginSchema,
+  updateSubscriptionSchema,
+} from "../schemas/authSchemas.js";
 
 const authRouter = express.Router();
 const jsonParser = express.json();
 
-authRouter.post("/register", jsonParser, AuthController.register);
-authRouter.post("/login", jsonParser, AuthController.login);
+authRouter.post(
+  "/register",
+  jsonParser,
+  validateBody(registerSchema),
+  AuthController.register
+);
+authRouter.post(
+  "/login",
+  jsonParser,
+  validateBody(loginSchema),
+  AuthController.login
+);
 authRouter.get("/logout", authMiddleware, AuthController.logout);
-authRouter.patch("/users", authMiddleware, AuthController.updateSubscription);
+authRouter.patch(
+  "/users",
+  authMiddleware,
+  validateBody(updateSubscriptionSchema),
+  AuthController.updateSubscription
+);
 
 export default authRouter;
