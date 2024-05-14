6d675fc1a9b971b5f4ff38ac978705bbd934e13e
Yarina Oleksii <aleksey.yarina@gmail.com>
2024-05-14 20:18:04 +0300

    upd: joi validation

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -1,10 +1,26 @@
+// import HttpError from "./HttpError.js";
+
+// const validateBody = (schema) => {
+//   const func = (req, _, next) => {
+//     const { error } = schema.validate(req.body);
+//     if (error) {
+//       next(HttpError(400, error.message));
+//     }
+//     next();
+//   };
+
+//   return func;
+// };
+
+// export default validateBody;
 import HttpError from "./HttpError.js";
 
 const validateBody = (schema) => {
-  const func = (req, _, next) => {
+  const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      const errorMessage = error.details[0].message;
+      return res.status(422).send({ message: errorMessage });
     }
     next();
   };
--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -1,18 +1,30 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
-  email: Joi.string().required(),
-  password: Joi.string().required(),
+  email: Joi.string().required().messages({
+    "any.required": "Email is required.",
+  }),
+  password: Joi.string().required().messages({
+    "any.required": "Password is required.",
+  }),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().required(),
-  password: Joi.string().required(),
+  email: Joi.string().required().messages({
+    "any.required": "Email is required.",
+  }),
+  password: Joi.string().required().messages({
+    "any.required": "Password is required.",
+  }),
 });
 
 const validSubscriptions = ["starter", "pro", "business"];
 export const updateSubscriptionSchema = Joi.object({
   subscription: Joi.string()
     .valid(...validSubscriptions)
-    .required(),
+    .required()
+    .messages({
+      "any.required": "Subscription type is required.",
+      "any.only": "Invalid subscription type.",
+    }),
 });
