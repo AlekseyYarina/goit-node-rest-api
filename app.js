d9446d899fdf915bcb6b25281e7746c22777507a
Yarina Oleksii <aleksey.yarina@gmail.com>
2024-05-25 16:22:53 +0300

    add: setting up nodemailer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import path from "node:path";
+import nodemailer from "nodemailer";
 
 import routes from "./routes/index.js";
 import "./db/db.js";
@@ -32,3 +33,22 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
 });
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USERNAME,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+});
+
+const message = {
+  to: "aleksey.yarina@gmail.com",
+  from: "aleksey.yarina@gmail.com",
+  subject: "Test nodemailer",
+  html: `<h1 style="color: red">Click on link a get a 90% discount on brand new iPhone 15 Pro Max</h1>`,
+  text: `Click on link a get a 90% discount on brand new iPhone 15 Pro Max`,
+};
+
+transport.sendMail(message).then(console.log).catch(console.error);
